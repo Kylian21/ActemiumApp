9d64d4c4cd81a0c11eb1a064c9284a4dc8c5f033
Louis HARISMENDY <louis.harismendy@gmail.com>
2020-04-17 16:41:06 +0200

    Auto stash before merge of "App" and "origin/App"

--- Flutter App/actemium_app/lib/EditPage/EditFormWidget.dart
+++ Flutter App/actemium_app/lib/EditPage/EditFormWidget.dart
@@ -20,7 +20,8 @@ class EditFormWidget extends StatefulWidget{
 
 class EditFormWidgetState extends State<EditFormWidget> {
   RegExp immatriculationRegExp = RegExp(
-      "[A-HJ-NP-TV-Z]{2}[\s-]{0,1}[0-9]{3}[\s-]{0,1}[A-HJ-NP-TV-Z]{2}|[0-9]{2,4}[\s-]{0,1}[A-Z]{1,3}[\s-]{0,1}[0-9]{2}");
+      '[A-HJ-NP-TV-Z]{2}[\s-]{0,1}[0-9]{3}[\s-]{0,1}[A-HJ-NP-TV-Z]{2}|[0-9]{2,4}[\s-]{0,1}[A-Z]{1,3}[\s-]{0,1}[0-9]{2}');
+  RegExp pinCodeRegExp = RegExp(r'^(?:\d{4}|\d{6})$');
 
   Widget build(BuildContext context) {
     print(widget.formKey.toString());
@@ -28,8 +29,12 @@ class EditFormWidgetState extends State<EditFormWidget> {
     return Form(
       key: widget.formKey,
       child: TextFormField(
-        keyboardType: widget.formKeyIndex==1 ? TextInputType.number:TextInputType.text,
-        textCapitalization: TextCapitalization.characters,
+        keyboardType: widget.formKeyIndex == 0
+            ? TextInputType.text
+            : TextInputType.number,
+        textCapitalization: widget.formKeyIndex == 0
+            ? TextCapitalization.characters
+            : TextCapitalization.none,
         inputFormatters: widget.formKeyIndex == 0
             ? <TextInputFormatter>[
                 MaskedTextInputFormatter(mask: 'xx-xxx-xx', separator: '-')
@@ -37,12 +42,16 @@ class EditFormWidgetState extends State<EditFormWidget> {
             : <TextInputFormatter>[],
         validator: (value) {
           if (widget.formKeyIndex == 0) {
+            print("oui");
             if (!immatriculationRegExp.hasMatch(value)) {
               return 'La plaque d\'immatriculation n\'est pas valide';
             } else
               return '';
-          }
-          else
+          } else if (widget.formKeyIndex == 1) {
+            if (!pinCodeRegExp.hasMatch(value)) {
+              return 'Le mot de passe n\'est pas sous forme de pin à 4 ou 6 chiffres';
+            }
+          } else
             return '';
         },
         cursorColor: Colors.blueGrey,
